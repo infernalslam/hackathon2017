09aac7b70e7d73804012b2a26358d35c12f82bb0
infernalslam <infernal-slam@hotmail.com>
2017-04-30 02:34:33 +0700

    add function

--- src/components/playlistFeed.vue
+++ src/components/playlistFeed.vue
@@ -1,8 +1,26 @@
 <template lang="html">
-  <div class="">
-    <div class="" v-for="show in store.state.playlistUser">
-      {{show.name}}
-      <img :src="show.img" alt="" width="20%">
+  <div class="container">
+    <div class="columns">
+      <div class="column is-3" v-for="show in store.state.playlistUser">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image">
+              <img :src="show.img" alt="Image">
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">Playlist :: {{show.name}}</p>
+              </div>
+            </div>
+          </div>
+          <footer class="card-footer">
+            <a class="card-footer-item" @click="add">Play</a>
+            <a class="card-footer-item" style="background: red; color: white;">Delete</a>
+          </footer>
+        </div>
+      </div>
     </div>
   </div>
 </template>
